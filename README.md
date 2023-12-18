8cbd479da128357eb2142cdd0ff87bdb84374a04
vinayreddy <vinayreddyu0159@gamil.com>
2023-12-18 19:17:31 +0530

    uploading files

---
+++ README.md
@@ -0,0 +1,2 @@
+# vinayredddy
+Protfolio
---
+++ assets/js/contact.js
@@ -0,0 +1,19 @@
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form');
+const contactMessage = document.getElementById('contact-message');
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ob0mcou', 'template_sw1novl', '#contact-form', 'vHWuA73qECr-XfBYr')
+        .then(() => {
+            contactMessage.textContent = "Message sent successfully ✅";
+
+            setTimeout(() => {
+                contactMessage.textContent = ""; 
+            }, 5000);
+        })
+
+};
+
+contactForm.addEventListener('submit', sendEmail);
