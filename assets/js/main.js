33d470123e6013520b065889fbe8003c0fa5871c
vinayreddy941 <vinayeee2001@gmail.com>
2023-12-23 23:27:31 +0530

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,13 +20,6 @@ if(navClose){
 }
 
 
-
-
-
-
-
-
-
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
 const bgHeader = () => {
@@ -38,41 +31,32 @@ const bgHeader = () => {
 
 window.addEventListener('scroll', bgHeader)
 /*=============== DARK LIGHT THEME ===============*/ 
-const themeButton = document.getElementById('theme-button');
-const darkTheme = 'dark-theme';
-const iconTheme = 'ri-sun-line';
 
-const selectedTheme = localStorage.getItem('selected-theme');
-const selectedIcon = localStorage.getItem('selected-icon');
 
-// Function to set the theme
-const setTheme = (theme, icon) => {
-    document.body.classList.add(theme);
-    themeButton.classList.add(icon);
-};
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
 
-// Function to toggle the theme
-const toggleTheme = () => {
-    document.body.classList.toggle(darkTheme);
-    themeButton.classList.toggle(iconTheme);
-};
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
 
-// Check if there's a selected theme in localStorage
-if (selectedTheme) {
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
-    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
-} else {
-    // If no theme is selected in localStorage, set dark theme as default
-    setTheme(darkTheme, 'ri-moon-line');
-    localStorage.setItem('selected-theme', 'dark');
-    localStorage.setItem('selected-icon', 'ri-moon-line');
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+
+
+if (selectedTheme){
+
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
 }
 
-themeButton.addEventListener('click', () => {
-    toggleTheme();
-    localStorage.setItem('selected-theme', getCurrentTheme());
-    localStorage.setItem('selected-icon', getCurrentIcon());
-});
 
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line';
+themeButton.addEventListener('click', () =>{
+
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme' , getCurrentTheme())
+    localStorage.setItem('selected-icon' , getCurrentIcon())
+
+})
