2b07fe4e53ef16097c121683f92aa6292be3fceb
vinayreddy941 <vinayeee2001@gmail.com>
2024-01-10 23:41:31 +0530

    updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,19 @@
-/*=============== SERVICES MODAL ===============*/
+
 const modal = document.querySelectorAll('.services__modal'),
-      modalButton = document.querySelectorAll('.services__button'), // Changed modalBotton to modalButtons
+      modalButton = document.querySelectorAll('.services__button'), 
       modalClose = document.querySelectorAll('.services__modal-close');
 
 let activeModal = (modalClick) => {
-    modal[modalClick].classList.add('active-modal'); // Removed the dot (.) before active-modal
+    modal[modalClick].classList.add('active-modal');
 };
 
-modalButton.forEach((modalButton, i) => { // Changed modalButton to modalButtons
+modalButton.forEach((modalButton, i) => { 
     modalButton.addEventListener('click', () => {
         activeModal(i);
     });
 });
 
 
-/*=============== SWIPER TESTIMONIAL ===============*/
-
-
-/*=============== SHOW SCROLL UP ===============*/ 
 
 
 const scrollUp = () => {
